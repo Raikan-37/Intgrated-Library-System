94dda41d9474f7b7f599f44d5efbb560411fa79e
Anik Mukherjee <52214455+Raikan-37@users.noreply.github.com>
2020-12-18 11:23:58 +0530

    Update borrower_class.py

--- borrower_class.py
+++ borrower_class.py
@@ -6,14 +6,6 @@ class Borrower:
         self.borrower_id = borrower_id
         self.book_id = book_id
         self.issue_date = issue_date
-      
-    # @property
-    # def email(self):
-    #     return '{}.{}@email.com'.format(self.first, self.last)
-
-    # @property
-    # def fullname(self):
-    #     return '{} {}'.format(self.first, self.last)
 
     def __repr__(self):
         return "Borrower('{}', {}, {}, '{}')".format(self.borrower_name, self.borrower_id, self.book_id, self.issue_date)
